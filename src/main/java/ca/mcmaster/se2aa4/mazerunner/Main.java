c72db28ff78092c36e4007853320ffe72f6ae459
JamieSteadman <jamie.nune@gmail.com>
2025-01-24 18:33:39 -0500

    Created basic groundwork for '-i' flag and updated print statements with logger calls

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -1,36 +1,82 @@
 package ca.mcmaster.se2aa4.mazerunner;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Main {
 
-    private static final Logger logger = LogManager.getLogger();
+    private static final Logger logger = LogManager.getLogger(); 
 
     public static void main(String[] args) {
-        System.out.println("** Starting Maze Runner");
+        Options options = new Options();
+        options.addOption("i", true, "Input file for maze path");
+
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd;
+        
+        logger.info("** Starting Maze Runner");
+
+        char [][] maze;
         try {
-            System.out.println("**** Reading the maze from file " + args[0]);
-            BufferedReader reader = new BufferedReader(new FileReader(args[0]));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                for (int idx = 0; idx < line.length(); idx++) {
-                    if (line.charAt(idx) == '#') {
-                        System.out.print("WALL ");
-                    } else if (line.charAt(idx) == ' ') {
-                        System.out.print("PASS ");
+            logger.info("**** Reading the maze from file " + args[0]);
+
+            cmd = parser.parse(options, args);
+
+            if (cmd.hasOption("i")) {
+                String fileInput = cmd.getOptionValue("i");
+                try (BufferedReader reader = new BufferedReader(new FileReader(fileInput))) {
+                    String line = reader.readLine();
+                    int counter = 0;
+                    int length = line.length();
+                    maze = new char[1][length]; //Initial size of maze array during reading
+
+                    while (line != null) { //Reading loop
+                        if (counter != 0) { 
+                            maze = resizeArray(maze, counter, length);
+                        }
+                        for (int i = 0; i < length; i++) {
+                            maze[counter][i] = line.charAt(i);
+                        }
+                        line = reader.readLine();
+                        counter++;
                     }
                 }
-                System.out.print(System.lineSeparator());
+            }
+            else {
+                BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    for (int idx = 0; idx < line.length(); idx++) {
+                        if (line.charAt(idx) == '#') {
+                            logger.trace("WALL ");
+                        } else if (line.charAt(idx) == ' ') {
+                            logger.trace("PASS ");
+                        }
+                    }
+                    logger.trace(System.lineSeparator());
+                }
             }
         } catch(Exception e) {
-            System.err.println("/!\\ An error has occured /!\\");
+            logger.error("/!\\ An error has occured /!\\");
         }
-        System.out.println("**** Computing path");
-        System.out.println("PATH NOT COMPUTED");
-        System.out.println("** End of MazeRunner");
+        logger.info("**** Computing path");
+        logger.info("PATH NOT COMPUTED");
+        logger.info("** End of MazeRunner");
+    }
+    private static char [][] resizeArray(char [][] arr, int height, int length) {
+        char [][] newArr = new char[height + 1][length];
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < height + 1; j++) {
+                newArr[i][j] = arr[i][j];
+            }
+        }
+        return newArr;
     }
 }
